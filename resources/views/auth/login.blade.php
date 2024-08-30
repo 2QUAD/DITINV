61172317dd4f787b1b30536218e4dded68aca367
2QUAD <ambxone@gmail.com>
2024-08-30 16:49:02 -0300

    Tradução da login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,19 +4,19 @@
 <div class="card card-md">
     <div class="card-body">
         <h2 class="h2 text-center mb-4">
-            Login to your account
+            INVENTARIO T.I JURU
         </h2>
         <form action="{{ route('login') }}" method="POST" autocomplete="off">
             @csrf
             <div class="mb-3">
                 <label for="email" class="form-label">
-                    Email address
+                    Email
                 </label>
                 <input type="email"
                        name="email"
                        id="email"
                        class="form-control @error('email') is-invalid @enderror"
-                       placeholder="your@email.com"
+                       placeholder="mail@email.com"
                        autocomplete="off"
                        value="{{ old('email') }}"
                 >
@@ -28,9 +28,9 @@
                 @enderror
             </div>
 
-            <div class="mb-2">
+            <div class="mb-3">
                 <label for="password" class="form-label">
-                    Password
+                    Senha
                 </label>
 
                 <div class="input-group input-group-flat">
@@ -38,7 +38,7 @@
                            name="password"
                            id="password"
                            class="form-control @error('password') is-invalid @enderror"
-                           placeholder="Your password"
+                           placeholder="Digite sua senha"
                            autocomplete="off"
                     >
 
@@ -53,25 +53,25 @@
             <div class="mb-2">
                 <label for="remember" class="form-check">
                     <input type="checkbox" id="remember" name="remember" class="form-check-input"/>
-                    <span class="form-check-label">Remember me on this device</span>
+                    <span class="form-check-label">Salvar sessão neste dispositivo</span>
                 </label>
             </div>
 
             <div class="form-footer">
                 <button type="submit" class="btn btn-primary w-100">
-                    Sign in
+                    Entrar
                 </button>
             </div>
         </form>
     </div>
 </div>
 <div class="text-center text-secondary mt-3">
-    Don't have account yet? <a href="{{ route('register') }}" tabindex="-1">
-        Sign up
+    Ainda não tem conta? <a href="{{ route('register') }}" tabindex="-1">
+       Inscrever-se
     </a>
 
     <span class="form-label-description">
-        <a href="{{ route('password.request') }}">I forgot password</a>
+        <a href="{{ route('password.request') }}">Esqueci a senha</a>
     </span>
 </div>
 @endsection
